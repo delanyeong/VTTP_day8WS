971254ff402615ff1d912a0e399222c16da93b51
delanyeong <delan.april@gmail.com>
2022-06-16 10:02:07 +0800

    first commit

---
+++ src/main/java/vttp2022/day08ws/App.java
@@ -0,0 +1,13 @@
+package vttp2022.day08ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
